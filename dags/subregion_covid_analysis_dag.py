65710f8b9802036213fb79bd58842152f1daf5e9
Vicky <hevictor@google.com>
2024-09-06 07:16:57 +0000

    Implementing dynamic variable reference in DAG

--- dags/subregion_covid_analysis_dag.py
+++ dags/subregion_covid_analysis_dag.py
@@ -17,11 +17,6 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-DAG_ID = "dataform_demo_workflow"
-PROJECT_ID = "TODO: project_id"
-REPOSITORY_ID = "quickstart-repository"
-REGION = "TODO: region"
-GIT_COMMITISH = "main"
 
 with DAG(
     "dataform_covid_analysis",
@@ -31,33 +26,35 @@ with DAG(
     start_date=datetime(2023, 1, 1),
     catchup=False,
 ) as dag:
-    
-    def get_dataform_config(environment):
+
+    def get_dataform_config():
         """
         Retrieves Dataform configuration based on the environment.
         """
         return {
-            'project_id': Variable.get(f'dataform_project_id'),
-            'region': Variable.get(f'dataform_region'),
-            'repository_id': Variable.get(f'dataform_repository_id'),
-            'git_commitish': Variable.get(f'dataform_environment'),
+            'project_id': Variable.get('dataform_project_id'),
+            'region': Variable.get('dataform_region'),
+            'repository_id': Variable.get('dataform_repository_id'),
+            'git_commitish': Variable.get('dataform_environment'),
         }
+
+    dataform_config = get_dataform_config()
 
     create_compilation = DataformCreateCompilationResultOperator(
         task_id="create_compilation",
-        project_id=PROJECT_ID,
-        region=REGION,
-        repository_id=REPOSITORY_ID,
+        project_id=dataform_config.get('project_id'),
+        region=dataform_config.get('region'),
+        repository_id=dataform_config.get('repository_id'),
         compilation_result={
-            "git_commitish": GIT_COMMITISH,
+            "git_commitish": dataform_config.get('git_commitish'),
         },
     )
 
     create_workflow_invocation = DataformCreateWorkflowInvocationOperator(
         task_id="create_workflow_invocation",
-        project_id=PROJECT_ID,
-        region=REGION,
-        repository_id=REPOSITORY_ID,
+        project_id=dataform_config.get('project_id'),
+        region=dataform_config.get('region'),
+        repository_id=dataform_config.get('repository_id'),
         workflow_invocation={
             "compilation_result": (
                 "{{ task_instance.xcom_pull('create_compilation')['name'] }}"
